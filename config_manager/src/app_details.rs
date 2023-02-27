c130e6cb7d084b6bc33ae7f4dc1a931f5abffac1
Midhun Kumar <midhunukumar5@gmail,com>
2023-02-28 00:46:09 +0530

    read from config file, and select option

---
+++ config_manager/src/app_details.rs
@@ -0,0 +1,34 @@
+use serde::{Deserialize, Serialize};
+use serde_yaml::{self};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct AppDetails{
+    pub name:String,
+    pub config_location:String,
+    pub config_files:Vec<String>
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Config{
+    pub source_location:String,
+    pub app_details:Vec<AppDetails>
+}
+
+pub fn read_config() -> Config{
+    let file = std::fs::File::open("config.yml")
+        .expect("Could not open the config file.");
+    let config : Config = serde_yaml::from_reader(file)
+        .expect("Could not read config values.");
+    return config;
+}
+
+pub fn count_and_display_options(app_details: &Vec<AppDetails>) -> usize{
+    let mut options_count: usize = 1;
+    for app_detail in app_details{
+        let app_name: &String = &app_detail.name;
+        let options = format!("{options_count} : {app_name}");
+        println!("{}",options);
+        options_count = options_count +1;
+    }
+    return options_count;
+}
--- config_manager/src/main.rs
+++ config_manager/src/main.rs
@@ -1,3 +1,21 @@
+pub mod app_details;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("debug:start");
+    let config: app_details::Config = app_details::read_config();
+    let app_count: usize = app_details::count_and_display_options(&config.app_details);
+
+    let mut user_input = String::new();
+    let stdin = io::stdin();
+    stdin.read_line(&mut user_input).expect("Error while reading user input");
+    let selected_option :usize = user_input.trim().parse().expect("Entered value is not a number");
+    if selected_option <=0 || selected_option > app_count{
+        println!("Select a valid option");
+        return;
+    }
+    let selected_app = &config.app_details[selected_option-1];
+    let app_name = &selected_app.name;
+    println!("{app_name}");
+    println!("debug:end");
 }
