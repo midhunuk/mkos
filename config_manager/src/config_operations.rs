8fcfbdad4c80cae679fd2d4d3e642845085aeb15
Midhun Kumar <midhunukumar5@gmail,com>
2023-03-07 01:13:00 +0530

    not tested function implementation for copy

--- config_manager/src/config_operations.rs
+++ config_manager/src/config_operations.rs
@@ -1,7 +1,10 @@
 use std::fmt;
 use std::path::Path;
+use std::fs;
+use std::path::PathBuf;
+use std::fs::File;
 
-use crate::app_details::AppDetails;
+use crate::app_details::{AppDetails};
 pub enum ConfigOperations {
     CopyFromSource = 1,
     CopyToSource
@@ -26,18 +29,31 @@ impl fmt::Display for ConfigOperations {
     }
 }
 
-pub fn perform_config_operation(selected_config_operation : ConfigOperations, app_details: Vec<&AppDetails>, repo_rooth_path: &String){
-    let (repo_file_paths, app_file_paths)  = get_file_paths(app_details, repo_rooth_path);
-    match selected_config_operation {
-        ConfigOperations::CopyFromSource => copy_files(repo_file_paths, app_file_paths),
-        ConfigOperations::CopyToSource => copy_files(app_file_paths, repo_file_paths),
+pub fn perform_config_operation(selected_config_operation : ConfigOperations, app_details: Vec<&AppDetails>, repo_root_path: &String){
+
+    for app_detail in app_details{
+        for config_file in &app_detail.config_files{
+            let repo_file_path = get_file_path(config_file, repo_root_path);
+            let app_file_path = get_file_path(config_file, &app_detail.config_location);
+            match selected_config_operation {
+                ConfigOperations::CopyFromSource => copy_file(repo_file_path, app_file_path),
+                ConfigOperations::CopyToSource => copy_file(app_file_path, repo_file_path),
+            }
+        }
     }
 }
 
-fn get_file_paths(app_details: Vec<&AppDetails>, source_path: &String) -> (Vec<String>, Vec<String>) {
-    todo!()
+fn get_file_path(file_path: &String, root_path: &String) -> PathBuf {
+    return Path::new(root_path).join(file_path);
 }
 
-fn copy_files(source_file_paths: Vec<String>, target_file_paths: Vec<String>) {
-    todo!()
+fn copy_file(source_file_path: PathBuf, target_file_path: PathBuf) {
+    if !source_file_path.exists(){
+        panic!("Source file does not exists");
+    }
+
+    if !target_file_path.exists(){
+        File::create(&target_file_path).expect("failed on creating file");
+    }
+    fs::copy(source_file_path, target_file_path).expect("Failed on copying files");
 }
