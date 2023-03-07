74223ac6115d0ca21c9679123b1443bbd9bb2e91
Midhun Kumar <midhunukumar5@gmail,com>
2023-03-07 22:51:57 +0530

    Added install script for config_manager

---
+++ config_manager/install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# DO not move or copy file to other location relative file path used 
+cargo clean
+cargo build --release
+
+sudo rm -rf /usr/local/bin/config_manager
+sudo cp target/release/config_manager /usr/local/bin/config_manager
+
+rm -rf ~/.config/config_manager/
+[ -d ~/.config/config_manager/ ] || mkdir ~/.config/config_manager/
+cp ../configs/config_manager/config.yml ~/.config/config_manager/config.yml
