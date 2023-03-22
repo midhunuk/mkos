804e5747ad632759792dd2a30762f08e578d9555
Midhun Kumar <midhunukumar5@gmail,com>
2023-03-22 20:27:33 +0530

    added lsp in nvim

--- apps/nvim/configs/lua/packer_init.lua
+++ apps/nvim/configs/lua/packer_init.lua
@@ -77,6 +77,30 @@ return packer.startup(function(use)
 	-- git plugin
 	use 'tpope/vim-fugitive'
 
+	-- lsp
+	use {
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v1.x',
+		requires = {
+			-- LSP Support
+			{'neovim/nvim-lspconfig'},             -- Required
+			{'williamboman/mason.nvim'},           -- Optional
+			{'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+			-- Autocompletion
+			{'hrsh7th/nvim-cmp'},         -- Required
+			{'hrsh7th/cmp-nvim-lsp'},     -- Required
+			{'hrsh7th/cmp-buffer'},       -- Optional
+			{'hrsh7th/cmp-path'},         -- Optional
+			{'saadparwaiz1/cmp_luasnip'}, -- Optional
+			{'hrsh7th/cmp-nvim-lua'},     -- Optional
+
+			-- Snippets
+			{'L3MON4D3/LuaSnip'},             -- Required
+			{'rafamadriz/friendly-snippets'}, -- Optional
+		}
+	}
+
 	-- Automatically set up your configuration after cloning packer.nvim
   	-- Put this at the end after all plugins
   	if PACKER_BOOTSTRAP then
