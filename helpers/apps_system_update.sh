4aab39d799fc68ca32e690f96c9ad2d0864d1f13
Midhun Kumar <midhunukumar5@gmail,com>
2023-06-08 22:46:45 +0530

    updated helper: system update

--- helpers/apps_system_update.sh
+++ helpers/apps_system_update.sh
@@ -1,7 +1,13 @@
+#!/bin/bash
 cd ~
 cd ~/git_repo/mkos/apps/apps_list/fedora
 dnf list installed > apps
 
+read -p "Enter commit message: " commit_message
+if [[ -z "$commit_message" ]]; then
+	commit_message="System update"
+fi
+
 git add apps
-git commit -m "system_update"
+git commit -m "$commit_message"
 
