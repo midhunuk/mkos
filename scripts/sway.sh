70424267b389313bab8426a764077c8d7d813392
Midhun Kumar <midhunukumar5@gmail,com>
2023-09-11 21:13:44 +0530

    splited sway config and added waybar

--- scripts/sway.sh
+++ scripts/sway.sh
@@ -8,3 +8,14 @@ cd $HOME
 
 sway_config_repo="$git_repo_location/configs/sway"
 ln -s "$sway_config_repo/config" $sway_config_location
+
+sway_config_d_location="$HOME/.config/sway/config.d"
+mkdir -p $sway_config_d_location
+
+sway_config_d_repo="$git_repo_location/configs/sway/config.d"
+ln -s "$sway_config_d_repo/workspaces" $sway_config_d_location
+
+sway_config_d_repo="$git_repo_location/configs/sway/config.d"
+ln -s "$sway_config_d_repo/statusbar" $sway_config_d_location
+
+
