1ecc33138327708e6d8fddb0b7d54e80a324b6ea
midhun kumar <midhunukumar5@gmail.com>
2023-10-31 14:31:46 +0530

    neovim installer script

---
+++ scripts/nvim-installer.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Build prerequisites
+apt-get install ninja-build gettext cmake unzip curl
+
+# package manager stow for nvim management
+apt-get install stow
+
+# building neovim
+cd /usr/local/src
+git clone https://github.com/neovim/neovim
+cd neovim 
+make CMAKE_BUILD_TYPE=RelWithDebInfo CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=/usr/local/stow/neovim" && make install
+
+cd ../../stow
+stow neovim
+
+apt-get ripgrep
