6765a3822231d28623736856ab3bc8081e989114
Midhun Kumar <midhunukumar@gmail.com>
2025-05-15 00:03:11 +0530

    Arch dev init

---
+++ arch/init.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+echo "installing apps"
+sudo pacman -S alacritty zsh git neovim stow bat
+
+echo "[*] Installing oh-my-zsh"
+sh -c "\$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+
+echo "[*] Stowing zsh config"
+cd ~
+stow -d ~/mkos -t ~ zsh
+
+echo "[*] Installing JetBrainsMono Nerd Font"
+mkdir -p ~/.local/share/fonts
+cd ~/.local/share/fonts
+wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.zip
+unzip JetBrainsMono.zip
+fc-cache -fv
+
+
+stow -d ~/mkos -t ~ alacritty
