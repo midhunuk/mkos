0e681aa4244720e48cfd433708bf4d1d74591b6e
Midhun Kumar <midhunukumar@gmail.com>
2025-05-15 21:07:12 +0530

    i3 wallpaper settings

--- i3/.config/i3/init.sh
+++ i3/.config/i3/init.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-sudo pacman -S i3-wm nitrogen 
+sudo pacman -S i3-wm feh
 
 # mkos already cloned and stow should be setup
 stow -d ~/mkos -t ~ i3 
---
+++ i3/.config/i3/wallpapers.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Wallpapers
+wallpapers_dir=/mnt/shared/git_repo/wallpapers
+
+#Monitors
+MONITORS=($(xrandr --query | grep " connected" | cut -d" " -f1))
+MONITOR_COUNT=${#MONITORS[@]}
+
+if [ "$MONITOR_COUNT" -gt 1 ]; then
+	wallpapers=($(find "$wallpapers_dir" -type f | shuf -n 2))
+	feh --no-fehbg --bg-scale "${wallpapers[0]}" "${wallpapers[1]}"
+else
+	wallpaper=$(find "$wallpapers_dir" -type f | shuf -n 1)
+	feh --no-fehbg --bg-scale "$random_wallpaper"
+fi
