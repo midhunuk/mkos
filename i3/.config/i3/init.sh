bd088d6ecd63c155ac58dfc6cb90978cbb47a12c
Midhun Kumar <midhunukumar@gmail.com>
2025-06-14 23:42:50 +0530

    i3 replaced xss-lock with xidlehook

--- i3/.config/i3/init.sh
+++ i3/.config/i3/init.sh
@@ -1,7 +1,8 @@
 #! /bin/sh
 
-sudo pacman -S i3-wm feh xss-lock i3lock polybar
+sudo pacman -S i3-wm feh i3lock polybar
 
+yay -S xidlehook betterlockscreen
 # mkos already cloned and stow should be setup
 stow -d ~/mkos -t ~ i3 
 stow -d ~/mkos -t ~ polybar
